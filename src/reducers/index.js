a04f50ed90a38fe300359cda4a90ebbf48ee4173
Артем Калинин <gennadijkalinin@MacBook-Pro.local>
2019-03-16 17:55:27 +0300

    123

---
+++ src/reducers/index.js
@@ -0,0 +1,44 @@
+import { combineReducers } from 'redux';
+
+export const initState = {
+  news: [
+    {
+        id: 1,
+        description: 'новость 1 из Redux'
+    }
+  ]
+};
+
+const mainReducer = (state = initState, action) => {
+
+    //С помощью конструкции switch case опишем каждый action
+    switch(action.type) {
+        case "Action_Add_News":
+            let arrNews= [...state.news]
+            arrNews.push({description: 'новая новость'});
+        return {
+            ...state,
+            news: arrNews
+        };  
+        case "Action_Uptade_News":
+            let arrEditNews = [...state.news];
+            arrEditNews[action.index].description = action.payload;
+            return {
+                ...state,
+               news: arrEditNews
+            };  
+        case "Action_Delete_News":
+            return {
+                ...state,
+               news: arrEditNews
+            };                   
+        default:
+            return state;       
+    }
+};
+
+export const todoApp = combineReducers ({
+    mainReducer: mainReducer
+});
+
+export default todoApp;
